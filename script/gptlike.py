45a87f8f8c3b5402e20d6bfcdf71d70647e6cc02
HijiriKawai <hiji.k1124@gmail.com>
2023-12-21 04:20:18 +0000

    update gptlike

--- script/gptlike.py
+++ script/gptlike.py
@@ -49,9 +49,7 @@ def main():
   data_cylinder = []
   data_wall = []
   target = []  # 教師データ
-  target_convex = []
-  target_cylinder = []
-  target_wall = []
+
 
   # 入力値と教師データを格納
   for x in range(0, 10):
@@ -62,9 +60,6 @@ def main():
           data_cylinder.append(tmp[::skip_num])
           data_wall.append(tmp[::skip_num])
           target.append(0)
-          target_convex.append(0)
-          target_cylinder.append(1)
-          target_wall.append(1)
       for i in range(csv_cylinder.shape[0]):
           tmp = csv_cylinder[i][length_start:length_end] + (0.1 * x)
           data.append(tmp[::skip_num])
@@ -72,9 +67,6 @@ def main():
           data_cylinder.append(tmp[::skip_num])
           data_wall.append(tmp[::skip_num])
           target.append(1)
-          target_convex.append(1)
-          target_cylinder.append(0)
-          target_wall.append(1)
       for i in range(csv_wall.shape[0]):
           tmp = csv_wall[i][length_start:length_end] + (0.1 * x)
           data.append(tmp[::skip_num])
@@ -82,9 +74,6 @@ def main():
           data_cylinder.append(tmp[::skip_num])
           data_wall.append(tmp[::skip_num])
           target.append(2)
-          target_convex.append(1)
-          target_cylinder.append(1)
-          target_wall.append(0)
 
 
   # 訓練データ、テストデータに分割
@@ -101,39 +90,33 @@ def main():
   )
 
   x_convex = np.array(data_convex).reshape(len(data_convex), int((length_end - length_start) / skip_num), 1)
-  t_convex = np.array(target_convex).reshape(len(target_convex), 1)
-  t_convex = np_utils.to_categorical(t_convex)  # 教師データをone-hot表現に変換
 
   # 訓練データ、検証データ、テストデータに分割
-  x_convex_train, x_convex_test, t_convex_train, t_convex_test = train_test_split(
-      x_convex, t_convex, test_size=int(len(data_convex) * 0.4), stratify=t_convex
+  x_convex_train, x_convex_test = train_test_split(
+      x_convex, test_size=int(len(data_convex) * 0.4)
   )
-  x_convex_valid, x_convex_test, t_convex_valid, t_convex_test = train_test_split(
-      x_convex_test, t_convex_test, test_size=int(len(x_convex_test) * 0.5), stratify=t_convex_test
+  x_convex_valid, x_convex_test = train_test_split(
+      x_convex_test, test_size=int(len(x_convex_test) * 0.5)
   )
 
   x_cylinder = np.array(data_cylinder).reshape(len(data_cylinder), int((length_end - length_start) / skip_num), 1)
-  t_cylinder = np.array(target_cylinder).reshape(len(target_cylinder), 1)
-  t_cylinder = np_utils.to_categorical(t_cylinder)  # 教師データをone-hot表現に変換
 
   # 訓練データ、検証データ、テストデータに分割
-  x_cylinder_train, x_cylinder_test, t_cylinder_train, t_cylinder_test = train_test_split(
-      x_cylinder, t_cylinder, test_size=int(len(data_cylinder) * 0.4), stratify=t_cylinder
+  x_cylinder_train, x_cylinder_test = train_test_split(
+      x_cylinder, test_size=int(len(data_cylinder) * 0.4)
   )
-  x_cylinder_valid, x_cylinder_test, t_cylinder_valid, t_cylinder_test = train_test_split(
-      x_cylinder_test, t_cylinder_test, test_size=int(len(x_cylinder_test) * 0.5), stratify=t_cylinder_test
+  x_cylinder_valid, x_cylinder_test = train_test_split(
+      x_cylinder_test, test_size=int(len(x_cylinder_test) * 0.5)
   )
 
   x_wall = np.array(data_wall).reshape(len(data_wall), int((length_end - length_start) / skip_num), 1)
-  t_wall = np.array(target_wall).reshape(len(target_wall), 1)
-  t_wall = np_utils.to_categorical(t_wall)  # 教師データをone-hot表現に変換
 
   # 訓練データ、検証データ、テストデータに分割
-  x_wall_train, x_wall_test, t_wall_train, t_wall_test = train_test_split(
-      x_wall, t_wall, test_size=int(len(data_wall) * 0.4), stratify=t_wall
+  x_wall_train, x_wall_test = train_test_split(
+      x_wall, test_size=int(len(data_wall) * 0.4)
   )
-  x_wall_valid, x_wall_test, t_wall_valid, t_wall_test = train_test_split(
-      x_wall_test, t_wall_test, test_size=int(len(x_test) * 0.5), stratify=t_wall_test
+  x_wall_valid, x_wall_test = train_test_split(
+      x_wall_test, test_size=int(len(x_test) * 0.5)
   )
 
   m_conf = GPTConfig(int((length_end - length_start) / skip_num), int((length_end - length_start) / skip_num), n_layer=12, n_head=8, n_embd=64)
@@ -165,34 +148,39 @@ def main():
 
   model_convex.fit(
       x_convex_train,
-      t_convex_train,
+      x_convex_train,
       batch_size=batch_size,
       epochs=epochs,
-      validation_data=(x_convex_valid, t_convex_valid),
+      validation_data=(x_convex_valid, x_convex_valid),
   )
 
   model_cylinder.fit(
       x_cylinder_train,
-      t_cylinder_train,
+      x_cylinder_train,
       batch_size=batch_size,
       epochs=epochs,
-      validation_data=(x_cylinder_valid, t_cylinder_valid),
+      validation_data=(x_cylinder_valid, x_cylinder_valid),
   )
 
   model_wall.fit(
       x_wall_train,
-      t_wall_train,
+      x_wall_train,
       batch_size=batch_size,
       epochs=epochs,
-      validation_data=(x_wall_valid, t_wall_valid),
+      validation_data=(x_wall_valid, x_wall_valid),
   )
 
-  model_convex = layers.Dense(1, activation="linear")(model_convex)
-  model_cylinder = layers.Dense(1, activation="linear")(model_cylinder)
-  model_wall = layers.Dense(1, activation="linear")(model_wall)
+  model_convex.trainable = False
+  model_cylinder.trainable = False
+  model_wall.trainable = False
+
+  inputs = layers.Input(Input(model_convex.input_shape[1 :]))
+  model_convex = layers.Dense(1, activation="linear")(model_convex(input))
+  model_cylinder = layers.Dense(1, activation="linear")(model_cylinder(input))
+  model_wall = layers.Dense(1, activation="linear")(model_wall(input))
   concatenated = layers.concatenate([model_convex, model_cylinder, model_wall])
-  result = layers.Dense(3, activation="softmax")(concatenated)
-  model = Model(inputs=[Input(model_convex.input_shape[1 :])], outputs=[result])
+  softmaxed = layers.Dense(3, activation="softmax")(concatenated)
+  model = Model(inputs=inputs, outputs=softmaxed)
 
   result = model.fit(
       x_train,
